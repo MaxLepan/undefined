62bbbd7c4e75f1daa7becaead0f5bc22cab94cc5
unknown <Administrateur@TURING-C10.formationara.local>
2021-11-15 09:08:58 +0100

    Changed gitignore

--- get_tweets/get_tweets.js
+++ get_tweets/get_tweets.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const Twitter = require('twitter');
-   const client = new Twitter({
+const client = new Twitter({
    consumer_key: process.env.TWITTER_CONSUMER_KEY,
    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,           
    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
@@ -16,7 +16,7 @@ stream.on('data', (data) => {
       const tweetText = data?.extended_tweet?.full_text || data.text;
       const tweetFrom = data?.place?.country_code || data.user?.location;
       const tweetLanguage = data?.lang || data.user?.lang;
-      console.log("@", tweetos, "\na tweeté ", tweetText, "\nà ", tweetFrom, '\nen', tweetLanguage );
+      console.log("@", tweetos, '\na tweeté "', tweetText, '"\nà', tweetFrom, '\nen', tweetLanguage, '\n=================================================================' );
    }
 });
 
