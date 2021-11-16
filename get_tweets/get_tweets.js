c547c259a2dc61703998709ccabdf780c492dec4
unknown <Administrateur@TURING-C10.formationara.local>
2021-11-16 11:21:05 +0100

    Working tweet number insertion in DB

--- get_tweets/get_tweets.js
+++ get_tweets/get_tweets.js
@@ -3,16 +3,15 @@ import dotenv from 'dotenv'
 dotenv.config();
 import Twitter from 'twitter';
 import { OpenAI } from 'openai';
-let express = require('http')
-const { MongoClient } = require("mongodb");
+import express from 'express'
 let app = express()
-let http = require("http");
+import {MongoClient} from "mongodb"
+import http from "http"
 let server = http.Server(app)
 
 const uri = "mongodb://" + process.env.USER + ":" + process.env.PASS + "@" + process.env.BDD_HOST + ":" + process.env.BDD_PORTS + "/?maxPoolSize=20&w=majority";
 
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
-const engine = await openai.getEngine('curie');
 
 const client = new Twitter({
    consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -24,21 +23,40 @@ const client = new Twitter({
 let positiveTweets
 let negativeTweets
 
+MongoClient.connect(uri, function (err, db) {
+   if (err) throw err;
+
+   var dbo = db.db("undefined");
+
+   dbo.collection("tweets").find({}).toArray(function(err, result) {
+      if (err) throw err;
+      console.log(result);
+
+      positiveTweets = result[0]['positive_tweets'];
+      negativeTweets = result[1]['negative_tweets'];
+
+      console.log("Positive tweets in DB : "+positiveTweets+"\nNegative tweets in DB : "+negativeTweets)
+
+      db.close();
+   });
+
+})
+
 async function classification (tweet) {
 
    try {
 
       const classificatedTweet = await openai.classify({
-      examples: [
-         ['A happy moment', 'Positive'],
-         ['I am sad.', 'Negative'],
-         ['I am feeling awesome', 'Positive']
-      ],
-      labels: ['Positive', 'Negative', 'Neutral'],
-      query: tweet,
-      search_model: 'curie',
-      model: 'curie'
-   });
+         examples: [
+            ['A happy moment', 'Positive'],
+            ['I am sad.', 'Negative'],
+            ['I am feeling awesome', 'Positive']
+         ],
+         labels: ['Positive', 'Negative', 'Neutral'],
+         query: tweet,
+         search_model: 'curie',
+         model: 'curie'
+      });
 
    console.log(classificatedTweet)
 
@@ -48,6 +66,24 @@ async function classification (tweet) {
       negativeTweets++
    }
 
+   console.log("Positive tweets : "+positiveTweets+"\nNegative tweets : "+negativeTweets)
+
+   MongoClient.connect(uri, function (err, db) {
+      if (err) throw err;
+
+      var dbo = db.db("undefined");
+
+      dbo.collection("tweets").updateMany({ }, 
+         { $set: { positive_tweets: positiveTweets, negative_tweets: negativeTweets }},
+         function(err, res) {
+            if (err) throw err;
+            console.log(res.matchedCount + " document(s) updated")
+            db.close();
+         }
+      )
+
+   });
+   
    console.log('==================================================================================================================================================')
    }
 
@@ -73,27 +109,3 @@ stream.on('data', (data) => {
 stream.on('error', (error) => {
    throw error;
 });
-
-
-function findAll(uri, database, collection, callback){
-
-   if (!String.isNullOrEmpty(database) && !String.isNullOrEmpty(collection)){
-
-      MongoClient.connect(uri, function (err, db) {
-         if (err) throw err;
-
-         var dbo = db.db(database);
-
-         dbo.collection(collection).find({}).toArray(function (err, result) {
-            if (err) throw err;
-               db.close();
-
-            if (typeof callback == "function" && callback != null)
-               callback(result);
-
-         });
-
-     });
-
-   }
-}
